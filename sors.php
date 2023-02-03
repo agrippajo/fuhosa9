b7a854876913abd05ce4311896e1a0922f66e74c
billy <123406277+agrippajo@users.noreply.github.com>
2023-02-03 19:35:49 +0200

    Create sors.php

---
+++ sors.php
@@ -0,0 +1,7 @@
+ account_binance = ccxt.binance({
+        'apiKey': API_KEY,
+        'secret': API_SECRET,
+        'enableRateLimit': True,
+        'options': {
+            'defaultType': 'spot'
+        }
